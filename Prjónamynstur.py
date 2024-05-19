7960534ae1b962f819ae42aed4f454870ada975b
Paolo Espiritu <paolo.v.espiritu@gmail.com>
2024-05-19 22:46:49 +0800

    additional problems solved

---
+++ Prjónamynstur.py
@@ -0,0 +1,15 @@
+def get_measurement(yarn):
+    if yarn == ".":
+        return 20
+    elif yarn == "o":
+        return 10
+    elif yarn == "\\":
+        return 25
+    elif yarn == "/":
+        return 25
+    elif yarn == "A":
+        return 35
+    elif yarn == "^":
+        return 5
+    elif yarn == "v":
+        return 22
