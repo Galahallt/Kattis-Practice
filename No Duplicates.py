ef8b33e069d5d010565c9624036fd64be748a2b6
Paolo Espiritu <paolo.v.espiritu@gmail.com>
2024-07-09 23:09:25 +0800

    additional problems solved

---
+++ No Duplicates.py
@@ -0,0 +1,13 @@
+import collections
+
+x = list(map(str, input().split()))
+j = 0
+for i in x:
+    if x.count(i) > 1:
+        print("no")
+        break
+    else:
+        j += 1
+
+if j == len(x):
+    print("yes")
